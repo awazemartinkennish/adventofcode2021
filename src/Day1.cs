211b35041a81073f1bdd06e5928b616e8917cb01
Martin Kennish <martin.kennish@awaze.co.uk>
2021-12-10 16:48:50 +0000

    day 1, part 1

---
+++ src/Day1.cs
@@ -0,0 +1,14 @@
+﻿namespace src;
+public class Day1
+{
+    public int Part1(List<int> inputs)
+    {
+        int count = 0;
+        for(int i = 1; i < inputs.Count; i++)
+        {
+            if (inputs[i] > inputs[i-1]) count++;
+        }
+
+        return count;
+    }
+}
